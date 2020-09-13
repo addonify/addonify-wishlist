fd28ad3babc33e4a758843c27485a9aa879752e9
Sanil Shakya <sonill_ss@hotmail.com>
2020-09-13 11:12:25 +0545

    first commit

---
+++ includes/class-addonify-wishlist-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://www.addonify.com
+ * @since      1.0.0
+ *
+ * @package    Addonify_Wishlist
+ * @subpackage Addonify_Wishlist/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Addonify_Wishlist
+ * @subpackage Addonify_Wishlist/includes
+ * @author     Adodnify <info@addonify.com>
+ */
+class Addonify_Wishlist_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-addonify-wishlist-i18n.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Define the internationalization functionality
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @link       https://www.addonify.com
+ * @since      1.0.0
+ *
+ * @package    Addonify_Wishlist
+ * @subpackage Addonify_Wishlist/includes
+ */
+
+/**
+ * Define the internationalization functionality.
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @since      1.0.0
+ * @package    Addonify_Wishlist
+ * @subpackage Addonify_Wishlist/includes
+ * @author     Adodnify <info@addonify.com>
+ */
+class Addonify_Wishlist_i18n {
+
+
+	/**
+	 * Load the plugin text domain for translation.
+	 *
+	 * @since    1.0.0
+	 */
+	public function load_plugin_textdomain() {
+
+		load_plugin_textdomain(
+			'addonify-wishlist',
+			false,
+			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
+		);
+
+	}
+
+
+
+}
