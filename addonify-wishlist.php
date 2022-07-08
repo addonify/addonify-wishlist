7f4502cddacdab0f170700f487f660b5412b3c8b
Ashok Lama <crazyhat17@gmail.com>
2022-07-08 15:33:14 +0545

    Changed readme.txt
    - Changed readme.txt
    - Added action hooks
    - Removed unused codes and files
    - Minor changes

--- addonify-wishlist.php
+++ addonify-wishlist.php
@@ -7,9 +7,9 @@
  * @package           Addonify_Wishlist
  *
  * @wordpress-plugin
- * Plugin Name:       Addonify WooCommerce Wishlist
+ * Plugin Name:       Addonify Wishlist
  * Plugin URI:        https://wordpress.org/plugins/addonify-wishlist
- * Description:       Addonify WooCommerce Wishlist is a light-weight yet powerful tool that adds a wishlist functionality to your e-commerce shop.
+ * Description:       Addonify Wishlist is a light-weight yet powerful tool that adds a wishlist functionality to your e-commerce shop.
  * Version:           1.0.0
  * Author:            Addodnify
  * Author URI:        https://www.addonify.com
