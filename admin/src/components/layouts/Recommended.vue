c78ab497a7d7496052052081529b693d59b44197
Anuj Subedi <struggleage@gmail.com>
2022-12-15 17:44:43 +0545

    :aerial_tramway: Added: Recommended plugin.
    @Todo:
    
    - Check plugin status.
    - Populate data & render.
    - User action.

---
+++ admin/src/components/layouts/Recommended.vue
@@ -0,0 +1,31 @@
+<script setup>
+	//import {ref, onMounted} from vue;
+	import { ElButton } from "element-plus";
+	import { Loading } from "@element-plus/icons-vue";
+
+	const props = defineProps({
+		slug: [Object, String],
+	});
+
+	const { slug } = props;
+	let title, description, buttonLabel;
+</script>
+
+<template>
+	<!--<div class="adfy-product-card">
+		<div class="adfy-product-box">
+			<figure class="adfy-product-thumb">
+				<img :src="getThumbnailWordPress(slug)" :alt="slug" />
+			</figure>
+			<div class="content">
+				<h3 class="adfy-product-title">{{ title }}</h3>
+				<p class="adfy-product-description" v-html="description"></p>
+				<div class="adfy-product-actions">
+					<el-button type="primary">
+						{{ buttonLabel !== "" ? buttonLabel : "Install" }}
+					</el-button>
+				</div>
+			</div>
+		</div>
+	</div>-->
+</template>
--- admin/src/views/Products.vue
+++ admin/src/views/Products.vue
@@ -1,6 +1,110 @@
 <script setup>
+	import { ref, onMounted } from "vue";
+	import Loading from "../components/layouts/Loading.vue";
 	import Navigation from "../components/layouts/Navigation.vue";
+	import Recommended from "../components/layouts/Recommended.vue";
+
+	let data = {};
 	let { __ } = wp.i18n;
+	let isLoading = ref(true);
+	let github =
+		"https://raw.githubusercontent.com/addonify/recommended-products/main/products.json";
+
+	/**
+	 *
+	 * Fetch the product list from the github repo.
+	 */
+
+	const fetchGithubRepo = async () => {
+		try {
+			const res = await fetch(github, {
+				// no cache.
+				cache: "no-cache",
+			});
+			data = await res.json();
+			console.log(data);
+
+			if (res.status !== 200) {
+				console.log("Couldn't fetch Github repo " + res);
+			}
+
+			processRecommendedPluginsList(data);
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
+	/**
+	 *
+	 * Get the recommended products.
+	 */
+
+	const processRecommendedPluginsList = (data) => {
+		let hotProducts = data.data.hot;
+
+		if (Array.isArray(hotProducts)) {
+			hotProducts.forEach((slug) => {
+				//console.log(addon);
+				checkPluginInstalled(slug);
+				fetchWpPluginInfo(slug);
+			});
+		}
+	};
+
+	/*
+	 *
+	 * Check plugin if plugin is installed on load.
+	 *
+	 */
+
+	const checkPluginInstalled = (slug) => {
+		console.log(`Checking ${slug} addon installed status.`);
+	};
+
+	/**
+	 *
+	 * WordPress.org/plugins get plugin thumbnail.
+	 */
+
+	const getThumbnailWordPress = (slug) => {
+		let thumbURL = `https://ps.w.org/${slug}/assets/icon-256x256.png`;
+		return thumbURL;
+	};
+
+	/**
+	 *
+	 * WordPress.org/plugins api get plugin info.
+	 */
+
+	const fetchWpPluginInfo = async (slug) => {
+		let wpApi = `https://api.wordpress.org/plugins/info/1.0/${slug}.json`;
+
+		try {
+			const res = await fetch(wpApi);
+			let data = await res.json();
+			console.log(data);
+
+			if (res.status !== 200) {
+				console.log("Couldn't fetch WordPress plugin repo " + res);
+			}
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
+	/*
+	 *
+	 * Handle install/activate button click action.
+	 * Called from Recommended.vue child component.
+	 */
+
+	const handleActivation = (slug) => {
+		console.log(slug);
+	};
+
+	onMounted(() => {
+		fetchGithubRepo();
+	});
 </script>
 
 <template>
@@ -10,7 +114,15 @@
 				<Navigation />
 			</aside>
 			<section class="adfy-col end site-primary">
-				<p>{{ __("Coming soon.....", "addonify-wishlist") }}</p>
+				<!--<Loading v-if="isLoading" />-->
+				<section id="recommended-products">
+					<div id="recommended-hot-products">
+						<div class="adfy-grid">
+							<!--<Recommended />-->
+						</div>
+					</div>
+					<div id="recommended-general-products"></div>
+				</section>
 			</section>
 		</main>
 	</section>
