a79321fb6bd8fbb1b905ec4b879d39d9af846581
Anuj Subedi <struggleage@gmail.com>
2023-03-24 14:06:45 +0545

    Removed: Unnecessary components.
    - Removed: Unnecessary components.

--- admin/app/src/components/options/OptionBoxHandler.vue
+++
@@ -1,6 +0,0 @@
-<script setup>
-import JumboBoxTitle from "@layouts/JumboBoxTitle.vue";
-import OptionBox from "@components/options/OptionBox.vue";
-const docLink = "https://docs.addonify.com/";
-</script>
-<template></template>
